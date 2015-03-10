9ca89434ca8bf64deb1b46032cd739050e3e7ef4
Jeffrey Morse <jam40jeff@gmail.com>
2015-03-10 08:07:07 -0400

    Fix nuspec.

--- Source/MorseCode.FrameworkExtensions/_Root/Properties/AssemblyInfo.cs
+++ Source/MorseCode.FrameworkExtensions/_Root/Properties/AssemblyInfo.cs
@@ -36,12 +36,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FrameworkExtensions")]
+[assembly: AssemblyTitle("MorseCode Framework Extensions")]
 [assembly: AssemblyDescription("Provides .NET framework extensions.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MorseCode Software")]
 [assembly: AssemblyProduct("MorseCode Framework Extensions")]
-[assembly: AssemblyCopyright("Copyright © MorseCode Software 2014")]
+[assembly: AssemblyCopyright("Copyright © MorseCode Software 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -63,5 +63,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
