d100dbfb8eb8589b5052afc7f3d42af6874367a9
Jeffrey Morse <jam40jeff@gmail.com>
2015-03-18 09:17:48 -0400

    Added SafeCast.

--- Source/MorseCode.FrameworkExtensions/_Root/ObjectExtensionMethods.cs
+++ Source/MorseCode.FrameworkExtensions/_Root/ObjectExtensionMethods.cs
@@ -32,14 +32,23 @@
 
 namespace MorseCode.FrameworkExtensions
 {
-    using System.Diagnostics.Contracts;
-
     /// <summary>
     /// Contains extension methods for use with any <see cref="object"/>.
     /// </summary>
     public static class ObjectExtensionMethods
     {
         #region Public Methods and Operators
+
+        /// <summary>
+        /// Safely casts an object to type <typeparamref name="T"/> using only an implicit conversion.
+        /// </summary>
+        /// <param name="o">The object to cast.</param>
+        /// <typeparam name="T">The type to which to cast.</typeparam>
+        /// <returns>The object as type <typeparamref name="T"/>.</returns>
+        public static T SafeCast<T>(this T o)
+        {
+            return o;
+        }
 
         /// <summary>
         /// Safely converts any object to a string, returning <code>null</code> if the object is <code>null</code> or calling the <see cref="object.ToString"/> method on it otherwise.
